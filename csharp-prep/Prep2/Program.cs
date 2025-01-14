a4875f9d7fde50f61684f6ca1bc0faf24a618e11
W0rkingB0tz <156626644+W0rkingB0tz@users.noreply.github.com>
2025-01-13 20:32:23 -0700

    Programming parts 1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        char letter;
+        bool passing;
+
+        string userInput;
+        Console.Write("What is your percent on the course? ");
+        userInput = Console.ReadLine();
+
+        int percent = int.Parse(userInput);
+
+        if (percent >= 90){
+            letter = 'A';
+            passing = true;
+        }
+        else if (percent >= 80){
+            letter = 'B';
+            passing = true;
+        }
+        else if (percent >= 70){
+            letter = 'C';
+            passing = true;
+        }
+        else if (percent >= 60){
+            letter = 'D';
+            passing = false;
+        }
+        else{
+            letter = 'F';
+            passing = false;
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (passing){
+            Console.WriteLine("Congratulations on passing the course!!!");
+        }
+        else{
+            Console.WriteLine("You can do even better next time.");
+        }
     }
 }
