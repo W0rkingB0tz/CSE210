6773bc550c520ad751f3d7de186fb1e85c1807b2
W0rkingB0tz <156626644+W0rkingB0tz@users.noreply.github.com>
2025-02-04 11:42:44 -0700

    Lesson 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction(5);
+        System.Console.WriteLine(fraction2.GetFractionString());
+        System.Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(3, 4);
+        System.Console.WriteLine(fraction3.GetFractionString());
+        System.Console.WriteLine(fraction3.GetDecimalValue());
+
+        Fraction fraction4 = new Fraction(1, 3);
+        System.Console.WriteLine(fraction4.GetFractionString());
+        System.Console.WriteLine(fraction4.GetDecimalValue());
     }
 }
